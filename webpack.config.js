8729ce69eb92daf4c6c0907c332f422d4dd440ab
ArpitSharma2800 <1arpit2000sharma@gmail.com>
2021-07-05 11:17:03 -0500

    added env support

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
-
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: './bundle.js',
+    filename: './bundle.js'
   },
   resolve: {
     modules: ['node_modules'],
@@ -14,33 +15,37 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            plugin: ['lodash']
-          },
-        }, ],
-      },
-    ],
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|jpe?g|gif)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          plugin: ['lodash']
+        }
+      }]
+    }
+    ]
   },
   devServer: {
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Production',
-      template: './public/index.html',
+      template: './public/index.html'
     }),
     new CaseSensitivePathsPlugin(),
-  ],
-};
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new Dotenv()
+  ]
+}
